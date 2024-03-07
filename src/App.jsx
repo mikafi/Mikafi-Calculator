f7efd24e5d43e23c9b7ff5ba7bf81d5a6a1e1813
romallemann <romain.allemann@gmail.com>
2024-03-07 15:57:02 +0100

    Max slider

--- src/App.jsx
+++ src/App.jsx
@@ -239,7 +239,7 @@ function App() {
                     label="500gr bags sold daily"
                     value={4}
                     min={0}
-                    max={10}
+                    max={30}
                     onChange={setExpectedBagsSold}
                   ></Slider>
 
